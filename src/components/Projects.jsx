fb643929ca89eaf6d0137afb973460b97b3123eb
Ivan <thecubeg@gmail.com>
2025-10-02 19:00:14 -0600

    feat: adding projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ export default function Projects() {
   return (
     <>
       <h2 className=" text-4xl font-bold text-left m-10">Projects:</h2>
-      <div className=" h-auto grid grid-cols-3 gap-5 mx-30 ">
+      <div className=" h-auto grid grid-cols-3 gap-5 mx-30 my-10">
         {projects.map((project) => {
           return <CardProject key={project.id} data={project} />;
         })}
